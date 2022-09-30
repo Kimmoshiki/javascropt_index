c53c310b6dfbd2c66d0a4cd178dfe54a0fd97193
kimhyunji <112531696+Kimmoshiki@users.noreply.github.com>
2022-09-30 12:14:25 +0900

    Add files via upload

---
+++ 0926/script/dom.js
@@ -0,0 +1,12 @@
+//dom.js
+
+let title01 = document.getElementsByTagName('h3');
+console.log(title01);
+
+for(let i=0;i<=title01.length;i++){
+    title01.item(i).style.color="#00f";
+}
+
+//2. id
+document.getElementById('p01').style.backgroundColor="#ff0";
+
